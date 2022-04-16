e0ee258e31ad5e56d454b15f264a75d3406566f7
jjant <jantonielli@itba.edu.ar>
2022-04-16 19:49:56 +0100

    Allow moving items around

--- src/bin/game.rs
+++ src/bin/game.rs
@@ -10,10 +10,14 @@ struct GameState {
     inventory: Inventory,
     mouse_x: i32,
     mouse_y: i32,
+    mouse_clicked: bool,
     highlighted_item: Option<usize>,
+    selected_item: Option<usize>,
 }
 enum Action {
     HighlightItem(usize),
+    SelectedItem(usize),
+    MovedSelectedItem { new_index: usize },
 }
 
 use Action::*;
@@ -25,10 +29,12 @@ impl App for GameState {
         Self {
             mouse_x: 64,
             mouse_y: 64,
+            mouse_clicked: false,
             player: Player::new(),
             inventory_open: true,
             inventory: Inventory::new(),
             highlighted_item: None,
+            selected_item: None,
         }
     }
 
@@ -38,6 +44,7 @@ impl App for GameState {
 
         self.mouse_x = state.mouse_x;
         self.mouse_y = state.mouse_y;
+        self.mouse_clicked = state.btnp(Button::Mouse);
 
         self.player.x += dx;
         self.player.y += dy;
@@ -48,8 +55,14 @@ impl App for GameState {
 
         self.highlighted_item = None;
         for action in actions {
-            match action {
-                &HighlightItem(index) => self.highlighted_item = Some(index),
+            match *action {
+                HighlightItem(index) => self.highlighted_item = Some(index),
+                SelectedItem(index) => self.selected_item = Some(index),
+                MovedSelectedItem { new_index } => {
+                    if let Some(selected_item) = self.selected_item {
+                        self.inventory.items.swap(selected_item, new_index);
+                    }
+                }
             }
         }
     }
@@ -62,22 +75,44 @@ impl App for GameState {
 
         if self.inventory_open {
             actions.append(&mut self.inventory.draw(
+                &self,
                 draw_context,
-                (self.mouse_x, self.mouse_y),
                 HighlightItem,
+                SelectedItem,
             ));
         }
 
         if let Some(item) = self
             .highlighted_item
-            .and_then(|index| self.inventory.items.get(index))
+            .and_then(|index| self.inventory.items[index].item.as_ref())
         {
             item.draw_tooltip(draw_context)
         }
 
-        draw_context.spr(56, self.mouse_x, self.mouse_y);
+        self.draw_cursor(draw_context);
 
         actions
+    }
+}
+
+impl GameState {
+    fn draw_cursor(&self, draw_context: &mut DrawContext) {
+        draw_context.spr(56, self.mouse_x, self.mouse_y);
+
+        let mut draw_selected_item = || {
+            let index = self.selected_item?;
+            let item = self.inventory.items[index].item.as_ref()?;
+
+            draw_context.spr(item.sprite as usize, self.mouse_x, self.mouse_y);
+
+            Some(())
+        };
+
+        draw_selected_item();
+    }
+
+    fn selected_item(&self) -> Option<&Item> {
+        self.inventory.items[self.selected_item?].item.as_ref()
     }
 }
 struct Player {
@@ -96,53 +131,60 @@ impl Player {
 }
 
 struct Inventory {
-    items: Vec<Item>,
+    items: Box<[ItemSlot]>,
 }
 
 impl Inventory {
     const START_X: i32 = 64 + 1;
     const START_Y: i32 = 64;
+
+    const ROWS: usize = 5;
+    const COLS: usize = 5;
     fn new() -> Self {
-        Self {
-            items: vec![
-                Item {
-                    name: "SWORD",
-                    description: "MELEE ATTACK",
-                    sprite: 48,
-                },
-                Item {
-                    name: "STAFF",
-                    description: "CASTS SPELLS",
-                    sprite: 51,
-                },
-            ],
-        }
+        let mut items = vec![ItemSlot { item: None }; Self::ROWS * Self::COLS].into_boxed_slice();
+        items[0] = ItemSlot {
+            item: Some(Item {
+                name: "SWORD",
+                description: "MELEE ATTACK",
+                sprite: 48,
+            }),
+        };
+        items[1] = ItemSlot {
+            item: Some(Item {
+                name: "STAFF",
+                description: "CASTS SPELLS",
+                sprite: 51,
+            }),
+        };
+
+        Self { items }
     }
 
     fn draw(
         &self,
+
+        game_state: &GameState,
         draw_context: &mut DrawContext,
-        mouse_pos: (i32, i32),
         on_hover: impl Fn(usize) -> Action,
+        on_click: impl Fn(usize) -> Action,
     ) -> Vec<Action> {
         let mut actions = vec![];
         draw_context.rectfill(64, 0, 128, 128, 6);
 
         for x_index in 0..5 {
             for y_index in 0..5 {
-                let x = Self::START_X + x_index * 12;
-                let y = Self::START_Y + y_index * 12;
-                draw_context.rectfill(x, y, x + 9, y + 9, 7);
+                let index = Self::index(x_index, y_index);
+                let item_slot = &self.items[index];
 
-                Self::item_rect(x_index as usize, y_index as usize).fill(draw_context, 5);
-
-                let index = (x_index + y_index * 5) as usize;
-                if let Some(item) = self.items.get(index) {
-                    if let Some(action) =
-                        item.draw_slot(draw_context, x, y, mouse_pos, on_hover(index))
-                    {
-                        actions.push(action);
-                    }
+                if let Some(action) = item_slot.draw(
+                    game_state,
+                    draw_context,
+                    x_index,
+                    y_index,
+                    on_hover(index),
+                    on_click(index),
+                ) {
+                    actions.push(action);
                 }
             }
         }
@@ -160,6 +202,55 @@ impl Inventory {
             height: 8,
         }
     }
+
+    pub fn index(x_index: usize, y_index: usize) -> usize {
+        x_index + y_index * 5
+    }
+}
+
+#[derive(Clone)]
+struct ItemSlot {
+    item: Option<Item>,
+}
+
+impl ItemSlot {
+    fn draw(
+        &self,
+        game_state: &GameState,
+        draw_context: &mut DrawContext,
+        x_index: usize,
+        y_index: usize,
+        on_hover: Action,
+        on_click: Action,
+    ) -> Option<Action> {
+        let border = 1;
+        let spacing = 1;
+        // 12 = 8 (sprite) + 2 (border) + 2 (spacing)
+        let x = Inventory::START_X + x_index as i32 * (8 + border * 2 + spacing * 2);
+        let y = Inventory::START_Y + y_index as i32 * (8 + border * 2 + spacing * 2);
+        draw_context.rectfill(x, y, x + 9, y + 9, 7);
+
+        let slot_rect = Inventory::item_rect(x_index, y_index);
+        slot_rect.fill(draw_context, 5);
+
+        match &self.item {
+            Some(item) => item.draw_slot(game_state, draw_context, x, y, on_hover, on_click),
+            None => {
+                let clicked = game_state.mouse_clicked
+                    && slot_rect.contains(game_state.mouse_x, game_state.mouse_y);
+                // move item to this slot
+                if clicked {
+                    let _ = game_state.selected_item?;
+
+                    Some(MovedSelectedItem {
+                        new_index: Inventory::index(x_index, y_index),
+                    })
+                } else {
+                    None
+                }
+            }
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -172,11 +263,12 @@ struct Item {
 impl Item {
     fn draw_slot(
         &self,
+        game_state: &GameState,
         draw_context: &mut DrawContext,
         x: i32,
         y: i32,
-        mouse_pos: (i32, i32),
         on_hover: Action,
+        on_click: Action,
     ) -> Option<Action> {
         draw_context.print(self.name, x, y, 8);
         draw_context.spr(self.sprite as usize, x + 1, y + 1);
@@ -187,9 +279,13 @@ impl Item {
             width: 8,
             height: 8,
         };
-
-        if sprite_rect.contains(mouse_pos.0, mouse_pos.1) {
-            Some(on_hover)
+        let contained = sprite_rect.contains(game_state.mouse_x, game_state.mouse_y);
+        if contained {
+            if game_state.mouse_clicked {
+                Some(on_click)
+            } else {
+                Some(on_hover)
+            }
         } else {
             None
         }
