cc6b5eb7d694befcbbe11deec46a57a83731dfe1
jjant <jantonielli@itba.edu.ar>
2022-05-23 23:10:58 +0100

    Implement player jump (incomplete)

--- src/bin/celeste.rs
+++ src/bin/celeste.rs
@@ -534,7 +534,9 @@ impl Player {
         base_object: &mut BaseObject,
         state: &State,
         objects: &mut impl Iterator<Item = &'a mut Object>,
+        got_fruit: &[bool],
         room: Vec2<i32>,
+        max_djump: i32,
     ) -> UpdateAction {
         let input = if state.btn(K_RIGHT) {
             1
@@ -543,9 +545,26 @@ impl Player {
         } else {
             0
         };
-
         let on_ground = base_object.is_solid(state, objects, room, 0, 1);
         let on_ice = base_object.is_ice(state, room, 0, 1);
+
+        let jump = state.btn(K_JUMP) && !self.p_jump;
+        self.p_jump = state.btn(K_JUMP);
+        if jump {
+            self.jbuffer = 4;
+        } else if self.jbuffer > 0 {
+            self.jbuffer -= 1;
+        }
+
+        if on_ground {
+            self.grace = 6;
+            if self.djump < max_djump {
+                //  psfx(54)
+                self.djump = max_djump;
+            }
+        } else if self.grace > 0 {
+            self.grace -= 1;
+        }
 
         // -- move
         // part
@@ -575,6 +594,81 @@ impl Player {
         // --facing
         if base_object.spd.x != 0.0 {
             base_object.flip.x = base_object.spd.x < 0.0;
+        }
+
+        // -- gravity
+        let mut maxfall = 2.0;
+        let mut gravity = 0.21;
+
+        if base_object.spd.y.abs() <= 0.15 {
+            gravity *= 0.5;
+        }
+
+        // -- wall slide
+        if input != 0
+            && base_object.is_solid(state, objects, room, input, 0)
+            && !base_object.is_ice(state, room, input, 0)
+        {
+            maxfall = 0.4;
+            if rnd(10.0) < 2.0 {
+                Object::init(
+                    got_fruit,
+                    room,
+                    ObjectKind::Smoke,
+                    base_object.x + input as f32 * 6.0,
+                    base_object.y,
+                    max_djump,
+                );
+            }
+        }
+
+        if !on_ground {
+            base_object.spd.y = appr(base_object.spd.y, maxfall, gravity);
+        }
+        // -- jump
+        if self.jbuffer > 0 {
+            if self.grace > 0 {
+                // normal jump
+                // psfx(1)
+                self.jbuffer = 0;
+                self.grace = 0;
+                base_object.spd.y -= 2.0;
+
+                let smoke = Object::init(
+                    got_fruit,
+                    room,
+                    ObjectKind::Smoke,
+                    base_object.x,
+                    base_object.y + 4.0,
+                    max_djump,
+                );
+            }
+            // -- wall jump
+            let wall_dir = if base_object.is_solid(state, objects, room, -3, 0) {
+                -1
+            } else if base_object.is_solid(state, objects, room, 3, 0) {
+                1
+            } else {
+                0
+            };
+
+            if wall_dir != 0 {
+                //psfx(2)
+                self.jbuffer = 0;
+                base_object.spd.y = -2.0;
+                base_object.spd.x = -wall_dir as f32 * (maxrun + 1.0);
+
+                if !base_object.is_ice(state, room, wall_dir * 3, 0) {
+                    let smoke = Object::init(
+                        got_fruit,
+                        room,
+                        ObjectKind::Smoke,
+                        base_object.x + (wall_dir * 6) as f32,
+                        base_object.y,
+                        max_djump,
+                    );
+                }
+            }
         }
 
         // Animation
@@ -628,204 +722,6 @@ struct HairElement {
     y: f32,
     size: i32,
 }
-
-// player =
-//     update=function(this)
-//         if (pause_player) return
-
-//         local input = btn(k_right) and 1 or (btn(k_left) and -1 or 0)
-
-//         -- spikes collide
-//         if spikes_at(this.x+this.hitbox.x,this.y+this.hitbox.y,this.hitbox.w,this.hitbox.h,this.spd.x,this.spd.y) then
-//          kill_player(this) end
-
-//         -- bottom death
-//         if this.y>128 then
-//             kill_player(this) end
-
-//         local on_ground=this.is_solid(0,1)
-//         local on_ice=this.is_ice(0,1)
-
-//         -- smoke particles
-//         if on_ground and not this.was_on_ground then
-//          init_object(smoke,this.x,this.y+4)
-//         end
-
-//         local jump = btn(k_jump) and not this.p_jump
-//         this.p_jump = btn(k_jump)
-//         if (jump) then
-//             this.jbuffer=4
-//         elseif this.jbuffer>0 then
-//          this.jbuffer-=1
-//         end
-
-//         local dash = btn(k_dash) and not this.p_dash
-//         this.p_dash = btn(k_dash)
-
-//         if on_ground then
-//             this.grace=6
-//             if this.djump<max_djump then
-//              psfx(54)
-//              this.djump=max_djump
-//             end
-//         elseif this.grace > 0 then
-//          this.grace-=1
-//         end
-
-//         this.dash_effect_time -=1
-//   if this.dash_time > 0 then
-//    init_object(smoke,this.x,this.y)
-//       this.dash_time-=1
-//       this.spd.x=appr(this.spd.x,this.dash_target.x,this.dash_accel.x)
-//       this.spd.y=appr(this.spd.y,this.dash_target.y,this.dash_accel.y)
-//   else
-
-//             -- move
-//             local maxrun=1
-//             local accel=0.6
-//             local deccel=0.15
-
-//             if not on_ground then
-//                 accel=0.4
-//             elseif on_ice then
-//                 accel=0.05
-//                 if input==(this.flip.x and -1 or 1) then
-//                     accel=0.05
-//                 end
-//             end
-
-//             if abs(this.spd.x) > maxrun then
-//              this.spd.x=appr(this.spd.x,sign(this.spd.x)*maxrun,deccel)
-//             else
-//                 this.spd.x=appr(this.spd.x,input*maxrun,accel)
-//             end
-
-//             --facing
-//             if this.spd.x!=0 then
-//                 this.flip.x=(this.spd.x<0)
-//             end
-
-//             -- gravity
-//             local maxfall=2
-//             local gravity=0.21
-
-//       if abs(this.spd.y) <= 0.15 then
-//        gravity*=0.5
-//             end
-
-//             -- wall slide
-//             if input!=0 and this.is_solid(input,0) and not this.is_ice(input,0) then
-//              maxfall=0.4
-//              if rnd(10)<2 then
-//                  init_object(smoke,this.x+input*6,this.y)
-//                 end
-//             end
-
-//             if not on_ground then
-//                 this.spd.y=appr(this.spd.y,maxfall,gravity)
-//             end
-
-//             -- jump
-//             if this.jbuffer>0 then
-//              if this.grace>0 then
-//               -- normal jump
-//               psfx(1)
-//               this.jbuffer=0
-//               this.grace=0
-//                     this.spd.y=-2
-//                     init_object(smoke,this.x,this.y+4)
-//                 else
-//                     -- wall jump
-//                     local wall_dir=(this.is_solid(-3,0) and -1 or this.is_solid(3,0) and 1 or 0)
-//                     if wall_dir!=0 then
-//                      psfx(2)
-//                      this.jbuffer=0
-//                      this.spd.y=-2
-//                      this.spd.x=-wall_dir*(maxrun+1)
-//                      if not this.is_ice(wall_dir*3,0) then
-//                          init_object(smoke,this.x+wall_dir*6,this.y)
-//                         end
-//                     end
-//                 end
-//             end
-
-//             -- dash
-//             local d_full=5
-//             local d_half=d_full*0.70710678118
-
-//             if this.djump>0 and dash then
-//              init_object(smoke,this.x,this.y)
-//              this.djump-=1
-//              this.dash_time=4
-//              has_dashed=true
-//              this.dash_effect_time=10
-//              local v_input=(btn(k_up) and -1 or (btn(k_down) and 1 or 0))
-//              if input!=0 then
-//               if v_input!=0 then
-//                this.spd.x=input*d_half
-//                this.spd.y=v_input*d_half
-//               else
-//                this.spd.x=input*d_full
-//                this.spd.y=0
-//               end
-//              elseif v_input!=0 then
-//                  this.spd.x=0
-//                  this.spd.y=v_input*d_full
-//              else
-//                  this.spd.x=(this.flip.x and -1 or 1)
-//               this.spd.y=0
-//              end
-
-//              psfx(3)
-//              freeze=2
-//              shake=6
-//              this.dash_target.x=2*sign(this.spd.x)
-//              this.dash_target.y=2*sign(this.spd.y)
-//              this.dash_accel.x=1.5
-//              this.dash_accel.y=1.5
-
-//              if this.spd.y<0 then
-//               this.dash_target.y*=.75
-//              end
-
-//              if this.spd.y!=0 then
-//               this.dash_accel.x*=0.70710678118
-//              end
-//              if this.spd.x!=0 then
-//               this.dash_accel.y*=0.70710678118
-//              end
-//             elseif dash and this.djump<=0 then
-//              psfx(9)
-//              init_object(smoke,this.x,this.y)
-//             end
-
-//         end
-
-//         -- animation
-//         this.spr_off+=0.25
-//         if not on_ground then
-//             if this.is_solid(input,0) then
-//                 this.spr=5
-//             else
-//                 this.spr=3
-//             end
-//         elseif btn(k_down) then
-//             this.spr=6
-//         elseif btn(k_up) then
-//             this.spr=7
-//         elseif (this.spd.x==0) or (not btn(k_left) and not btn(k_right)) then
-//             this.spr=1
-//         else
-//             this.spr=1+this.spr_off%4
-//         end
-
-//         -- next level
-//         if this.y<-4 and level_index()<30 then next_room() end
-
-//         -- was on the ground
-//         this.was_on_ground=on_ground
-
-//     end, --<end update loop
 
 #[allow(dead_code, unused_variables)]
 fn psfx(game_state: &GameState, num: i32) {
@@ -1460,6 +1356,7 @@ fn got_fruit_for_room(got_fruit: &[bool], room: Vec2<i32>) -> bool {
 }
 
 impl Object {
+    #[must_use]
     fn init(
         got_fruit: &[bool],
         room: Vec2<i32>,
@@ -1725,7 +1622,14 @@ impl ObjectType {
             ObjectType::Smoke => Smoke::update(base_object),
             ObjectType::Platform => todo!(),
             // ObjectType::BigChest => todo!(),
-            ObjectType::Player(player) => player.update(base_object, state, other_objects, room),
+            ObjectType::Player(player) => player.update(
+                base_object,
+                state,
+                other_objects,
+                got_fruit,
+                room,
+                max_djump,
+            ),
             // ObjectType::LifeUp => todo!(),
             ObjectType::Fruit(fruit) => {
                 fruit.update(base_object, other_objects, got_fruit, room, max_djump)
