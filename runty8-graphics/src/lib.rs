a88e51be63542c3e2d5039e63e1847a0a3fb3b85
Julian Antonielli <julianantonielli@gmail.com>
2022-08-26 14:43:11 +0100

    Implement filled rectangle in terms of horizontal line

--- runty8-graphics/src/lib.rs
+++ runty8-graphics/src/lib.rs
@@ -1,4 +1,12 @@
-pub fn rect(x: i32, y: i32, width: u32, height: u32) -> impl Iterator<Item = (i32, i32)> {
+pub trait Graphics: Iterator<Item = (i32, i32)> {}
+
+impl<Type: Iterator<Item = (i32, i32)>> Graphics for Type {}
+
+fn horizontal_line(x0: i32, x1: i32, y: i32) -> impl Graphics {
+    (x0..x1).map(move |x| (x, y))
+}
+
+pub fn filled_rectangle(x: i32, y: i32, width: u32, height: u32) -> impl Graphics {
     let width: i32 = width
         .try_into()
         .expect(&format!("Couldn't convert width {} to i32", width));
@@ -7,7 +15,7 @@ pub fn rect(x: i32, y: i32, width: u32, height: u32) -> impl Iterator<Item = (i3
         .try_into()
         .expect(&format!("Couldn't convert height {} to i32", height));
 
-    (y..y + height).flat_map(move |y| (x..x + width).map(move |x| (x, y)))
+    (y..y + height).flat_map(move |y| horizontal_line(x, x + width, y))
 }
 
 #[cfg(test)]
@@ -16,17 +24,17 @@ mod tests {
 
     #[test]
     fn empty_rectangle() {
-        assert_eq!(rect(34, 22, 0, 0).count(), 0);
+        assert_eq!(filled_rectangle(34, 22, 0, 0).count(), 0);
     }
 
     #[test]
     fn small_rectangle_count() {
-        assert_eq!(rect(8, 4, 2, 3).count(), 6);
+        assert_eq!(filled_rectangle(8, 4, 2, 3).count(), 6);
     }
 
     #[test]
     fn small_rectangle() {
-        let mut rectangle = rect(8, 4, 2, 3);
+        let mut rectangle = filled_rectangle(8, 4, 2, 3);
 
         assert_eq!(rectangle.next(), Some((8, 4)));
         assert_eq!(rectangle.next(), Some((9, 4)));
