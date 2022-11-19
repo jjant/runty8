8f1b90709874f7ed912e55923acb100099478e98
Julian Antonielli <julianantonielli@gmail.com>
2022-11-19 18:47:50 +0000

    Move stuff from `runty8-run` to `runty8-event-loop`

--- runty8-run/src/lib.rs
+++ runty8-run/src/lib.rs
@@ -1,7 +1,6 @@
 mod event;
 mod graphics;
 
-use event::{Event, MouseButton, MouseEvent};
 use glium::backend::Facade;
 use glium::glutin::dpi::{LogicalPosition, LogicalSize};
 use glium::glutin::event::{self as glutin_event, ElementState, KeyboardInput};
@@ -12,6 +11,7 @@ use glium::uniforms::{MagnifySamplerFilter, Sampler};
 use glium::{glutin, Display, Program, Surface};
 use glium::{uniform, Frame};
 use graphics::{whole_screen_vertex_buffer, FRAGMENT_SHADER, VERTEX_SHADER};
+use runty8_event_loop::{Event, MouseButton, MouseEvent};
 use runty8_runtime::{App, Key, KeyState, KeyboardEvent, Pico8, Resources};
 
 pub fn run_app<Game: App + 'static>(resources: Resources) {
@@ -55,91 +55,6 @@ pub fn run_app<Game: App + 'static>(resources: Resources) {
             &program,
         );
     });
-}
-
-/// Translates a glutin::event::Event into a runty8 Event.
-fn translate_event(
-    event: &glutin_event::Event<()>,
-    hidpi_factor: f64,
-    window_size: &mut LogicalSize<f64>,
-    control_flow: &mut ControlFlow,
-) -> Option<Event> {
-    match event {
-        glutin_event::Event::WindowEvent { event, .. } => match event {
-            glutin_event::WindowEvent::CloseRequested => {
-                *control_flow = glutin::event_loop::ControlFlow::Exit;
-
-                None
-            }
-            // TODO: Force aspect ratio on resize.
-            &glutin::event::WindowEvent::Resized(new_size) => {
-                let new_size: LogicalSize<f64> = new_size.to_logical(hidpi_factor);
-
-                *window_size = new_size;
-
-                None
-            }
-            glutin::event::WindowEvent::CursorMoved { position, .. } => {
-                let logical_mouse: LogicalPosition<f64> = position.to_logical(hidpi_factor);
-
-                Some(Event::Mouse(MouseEvent::Move {
-                    x: (logical_mouse.x / window_size.width * 128.).floor() as i32,
-                    y: (logical_mouse.y / window_size.height * 128.).floor() as i32,
-                }))
-            }
-            glutin::event::WindowEvent::MouseInput {
-                button: glutin_event::MouseButton::Left,
-                state: input_state,
-                ..
-            } => {
-                let mouse_event = match input_state {
-                    ElementState::Pressed => MouseEvent::Down(MouseButton::Left),
-                    ElementState::Released => MouseEvent::Up(MouseButton::Left),
-                };
-
-                Some(Event::Mouse(mouse_event))
-            }
-            glutin::event::WindowEvent::KeyboardInput { input, .. } => {
-                handle_keyboard_event(input).map(Event::Keyboard)
-            }
-            _ => None,
-        },
-        glutin_event::Event::NewEvents(cause) => match cause {
-            glutin::event::StartCause::ResumeTimeReached {
-                start,
-                requested_resume,
-            } => {
-                set_next_timer(control_flow);
-
-                let delta: Result<i32, _> = requested_resume
-                    .duration_since(*start)
-                    .as_millis()
-                    .try_into();
-
-                Some(Event::Tick {
-                    delta_millis: delta.unwrap().try_into().unwrap(),
-                })
-            }
-            glutin::event::StartCause::Init => {
-                set_next_timer(control_flow);
-
-                None
-            }
-            _ => None,
-        },
-        _ => None,
-    }
-}
-
-fn handle_keyboard_event(input: &KeyboardInput) -> Option<KeyboardEvent> {
-    let key = input.virtual_keycode?;
-    let runty8_key = Key::from_virtual_keycode(key)?;
-    let state = KeyState::from_state(input.state);
-
-    Some(KeyboardEvent {
-        key: runty8_key,
-        state,
-    })
 }
 
 fn set_next_timer(control_flow: &mut ControlFlow) {
