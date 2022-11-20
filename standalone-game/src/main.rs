f00bd4744d06b6f818dbbb608bb1d6447296e315
Julian Antonielli <julianantonielli@gmail.com>
2022-11-20 15:50:46 +0000

    Use keyboard/mouse input in demo

--- standalone-game/src/main.rs
+++ standalone-game/src/main.rs
@@ -1,5 +1,7 @@
 use rand::Rng;
-use runty8_runtime::{App, Flags, Map, Pico8, Resources, SpriteSheet};
+use runty8_runtime::{App, Button, Flags, Map, Pico8, Resources, SpriteSheet};
+
+const LOG: bool = false;
 
 fn main() {
     unsafe {
@@ -12,32 +14,80 @@ fn main() {
     }
 }
 
-struct Game;
+struct Game {
+    x: i32,
+    y: i32,
+    mouse_x: i32,
+    mouse_y: i32,
+}
+
+impl Game {
+    const W: i32 = 3;
+    const H: i32 = 3;
+}
 
 impl App for Game {
     fn init(pico8: &mut Pico8) -> Self {
         log::debug!("Game::init called");
         pico8.rect(15, 15, 30, 30, 8);
         pico8.set_title("nice".to_string());
-        Self
+
+        Self {
+            x: 64,
+            y: 64,
+            mouse_x: 64,
+            mouse_y: 64,
+        }
     }
 
     fn update(&mut self, pico8: &mut Pico8) {
-        log::debug!("Game::update called");
-        println!("Game::update called");
-        //   pico8.cls(0);
-        pico8.print("SOMETHING", 8, 8, 7);
+        if LOG {
+            log::debug!("Game::update called");
+            println!("Game::update called");
+        }
+
+        if pico8.btn(Button::Down) {
+            self.y += 1
+        } else if pico8.btn(Button::Up) {
+            self.y -= 1
+        }
+        if pico8.btn(Button::Right) {
+            self.x += 1
+        } else if pico8.btn(Button::Left) {
+            self.x -= 1
+        }
+        self.x = clamp(0, self.x, 128 - Self::W);
+        self.y = clamp(0, self.y, 128 - Self::H);
+
+        (self.mouse_x, self.mouse_y) = pico8.mouse();
     }
 
     fn draw(&mut self, pico8: &mut Pico8) {
-        log::debug!("Game::draw called");
-        println!("Game::draw called");
-        pico8.rect(
-            rand::thread_rng().gen_range(0..128),
-            rand::thread_rng().gen_range(0..128),
-            rand::thread_rng().gen_range(0..128),
-            rand::thread_rng().gen_range(0..128),
-            8,
+        if LOG {
+            log::debug!("Game::draw called");
+            println!("Game::draw called");
+        }
+        pico8.cls(0);
+
+        for i in 0..15 {
+            let x = i % 4;
+            let y = i / 4;
+
+            pico8.rectfill(x * 4, y * 4, x * 4 + 3, y * 4 + 3, i as u8);
+        }
+        pico8.print("USE ARROW KEYS TO MOVE", 8, 8, 12);
+        pico8.rectfill(
+            self.x,
+            self.y,
+            self.x + (Self::W - 1),
+            self.y + (Self::H - 1),
+            7,
         );
+
+        pico8.pset(self.mouse_x, self.mouse_y, 9);
     }
+}
+
+fn clamp(min: i32, v: i32, max: i32) -> i32 {
+    runty8_runtime::mid(min as f32, v as f32, max as f32) as i32
 }
