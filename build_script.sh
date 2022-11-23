b817df1c69cbc7ec4b92105d6f81b5ddb43e56cc
Julian Antonielli <julianantonielli@gmail.com>
2022-11-23 22:41:16 +0000

    Make `build_script.sh` work with other examples

--- build_script.sh
+++ build_script.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-cargo build --target wasm32-unknown-unknown -p standalone-game
-wasm-bindgen target/wasm32-unknown-unknown/debug/standalone-game.wasm --out-dir generated --target web
+package="standalone-game"
+
+rm -rf generated/*
+cargo build --target wasm32-unknown-unknown -p "$package"
+wasm-bindgen target/wasm32-unknown-unknown/debug/$package.wasm --out-dir generated --target web
 cp index.html generated/index.html
 cd generated
 serve
