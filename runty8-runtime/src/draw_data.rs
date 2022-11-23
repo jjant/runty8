dbed6938faa4824c4e8779f59224b7d59d81db35
Julian Antonielli <julianantonielli@gmail.com>
2022-11-23 20:58:55 +0000

    Add color names to color literals

--- runty8-runtime/src/draw_data.rs
+++ runty8-runtime/src/draw_data.rs
@@ -325,22 +325,22 @@ fn get_color(index: Color) -> u32 {
 
 // Add _FF at the end for alpha
 pub const COLORS: [u32; 16] = [
-    0x000000, //
-    0x1D2B53, //
-    0x7E2553, //
-    0x008751, //
-    0xAB5236, //
-    0x5F574F, //
-    0xC2C3C7, //
-    0xFFF1E8, //
-    0xFF004D, //
-    0xFFA300, //
-    0xFFEC27, //
-    0x00E436, //
-    0x29ADFF, //
-    0x83769C, //
-    0xFF77A8, //
-    0xFFCCAA, //
+    0x000000, // Black
+    0x1D2B53, // Dark blue
+    0x7E2553, // Dark purple
+    0x008751, // Dark green
+    0xAB5236, // Brown
+    0x5F574F, // Dark grey
+    0xC2C3C7, // Light grey
+    0xFFF1E8, // White
+    0xFF004D, // Red
+    0xFFA300, // Orange
+    0xFFEC27, // Yellow
+    0x00E436, // Green
+    0x29ADFF, // Blue
+    0x83769C, // Lavender
+    0xFF77A8, // Pink
+    0xFFCCAA, // Light peach
 ];
 
 /// Named constants for the colors in the Pico8 palette.
