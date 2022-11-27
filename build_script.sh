ce02af687cc976230667ca4af526d09ed5b870e6
Julian Antonielli <julianantonielli@gmail.com>
2022-11-27 15:24:46 +0000

    Use `release` mode in `build_script.sh`

--- build_script.sh
+++ build_script.sh
@@ -11,8 +11,8 @@ fi
 echo "Building: $package"
 
 rm -rf generated/*
-cargo build --target wasm32-unknown-unknown -p "$package" --quiet
-wasm-bindgen target/wasm32-unknown-unknown/debug/$package.wasm --out-dir generated --target web
+cargo build --target wasm32-unknown-unknown -p "examples" --bin "$package" --release
+wasm-bindgen target/wasm32-unknown-unknown/release/$package.wasm --out-dir generated --target web
 
 cp index.html generated/index.html
 placeholder="__PACKAGE_NAME__"
