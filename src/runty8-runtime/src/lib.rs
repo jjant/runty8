d89b7a2e98388190ed1e287244cd60a4b2f6d5a2
Julian Antonielli <julianantonielli@gmail.com>
2022-12-03 12:52:15 +0000

    Fix timestep in `runty8-runtime`
    Applies the "Variable delta time" strategy described here:
    https://gafferongames.com/post/fix_your_timestep/

--- src/runty8-runtime/src/lib.rs
+++ src/runty8-runtime/src/lib.rs
@@ -8,16 +8,25 @@ pub fn run<Game: App + 'static>(resources: Resources) -> std::io::Result<()> {
     let mut game = Game::init(&mut pico8);
     let mut input = Input::new();
 
+    const DELTA_TIME: f64 = 1000.0 / 30.0;
+
+    let mut accumulated_delta = 0.0;
     let on_event = move |event,
                          control_flow: &mut ControlFlow,
                          draw: &dyn Fn(&[u8], &mut ControlFlow)| match event {
-        Event::Tick { .. } => {
-            pico8.state.update_input(&input);
+        Event::Tick { delta_millis } => {
+            accumulated_delta += delta_millis;
 
-            game.update(&mut pico8);
-            game.draw(&mut pico8);
+            while accumulated_delta > DELTA_TIME {
+                pico8.state.update_input(&input);
 
-            draw(pico8.draw_data.buffer(), control_flow);
+                game.update(&mut pico8);
+                game.draw(&mut pico8);
+
+                draw(pico8.draw_data.buffer(), control_flow);
+
+                accumulated_delta -= DELTA_TIME;
+            }
         }
         Event::Input(input_event) => {
             input.on_event(input_event);
