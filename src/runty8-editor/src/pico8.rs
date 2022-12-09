302b60e03b2dfe2ad3f7b82bd9ef0bf1a7c35596
Julian Antonielli <julianantonielli@gmail.com>
2022-12-09 12:48:32 +0000

    Add extension trait to render editor sprites

---
+++ src/runty8-editor/src/pico8.rs
@@ -0,0 +1,26 @@
+//! Extension trait to render editor sprites.
+
+use once_cell::sync::Lazy;
+use runty8_core::{Pico8, SpriteSheet};
+
+/// Extension trait to render editor sprites.
+pub(crate) trait Pico8EditorExt {
+    fn editor_spr(&mut self, spr: usize, x: i32, y: i32);
+}
+
+impl Pico8EditorExt for Pico8 {
+    fn editor_spr(&mut self, spr: usize, x: i32, y: i32) {
+        let sprite = editor_sprite_sheet().get_sprite(spr);
+
+        self.raw_spr(sprite, x, y, 1.0, 1.0, false, false);
+    }
+}
+
+/// Lazily parse sprite sheet.
+fn editor_sprite_sheet() -> &'static SpriteSheet {
+    static SPRITE_SHEET_BYTES: &str = include_str!("./editor_assets/sprite_sheet.txt");
+    static SPRITE_SHEET: Lazy<SpriteSheet> =
+        Lazy::new(|| SpriteSheet::deserialize(SPRITE_SHEET_BYTES).unwrap());
+
+    &SPRITE_SHEET
+}
