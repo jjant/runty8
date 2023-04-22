ef83101f59be07d743ad72d5f6d09eef9f30f4e3
lesleyrs <19632758+lesleyrs@users.noreply.github.com>
2023-04-22 10:46:39 +0200

    remove if check

--- src/runty8-core/build.rs
+++ src/runty8-core/build.rs
@@ -1,8 +1,5 @@
 extern crate embed_resource;
 
 fn main() {
-    let target = std::env::var("TARGET").unwrap();
-    if target.contains("windows") {
-        embed_resource::compile("icon.rc", embed_resource::NONE);
-    }
+    embed_resource::compile("icon.rc", embed_resource::NONE);
 }
